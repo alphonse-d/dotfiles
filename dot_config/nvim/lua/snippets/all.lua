237310ce1ea9c3c3885aa9bc0541e41c2bc81027
Alphonse <ado@epic.com>
2026-03-12 13:04:36 -0500

    Add date snippets

--- dot_config/nvim/lua/snippets/all.lua
+++ dot_config/nvim/lua/snippets/all.lua
@@ -2,7 +2,21 @@ local ls = require("luasnip")
 local s = ls.snippet
 local f = ls.function_node
 
-return {}, {
+return {
+-- Date filler, can be filled in for more fillings
+  s({ trig = "@today" }, f(function()
+    return os.date("%Y-%m-%d")
+  end)),
+  s({ trig = "@tomorrow" }, f(function()
+    return os.date("%Y-%m-%d", os.time() + 86400)
+  end)),
+  s({ trig = "@friday" }, f(function()
+    local today = tonumber(os.date("%w"))
+    local diff = (5 - today) % 7
+    if diff == 0 then diff = 7 end
+    return os.date("%Y-%m-%d", os.time() + diff * 86400)
+  end)),
+}, {
   s({
     trig = "(SLG)%.(%d+) ",
     regTrig = true,
